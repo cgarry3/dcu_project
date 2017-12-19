4cd13f46c9f715bf1b80041ba27c9b9d267e0a17
cgarry3 <cathal.garry3@mail.dcu.ie>
2017-12-19 16:35:04 +0000

    Reduce image size down to 720P

--- design/hls/rgb2grayScale.h
+++ design/hls/rgb2grayScale.h
@@ -4,8 +4,8 @@
 	#include  "hls_video.h"
 	#include <ap_fixed.h>
 
-    #define MAX_WIDTH  1920
-    #define MAX_HEIGHT 1080
+    #define MAX_WIDTH  1280
+    #define MAX_HEIGHT 720
 
     typedef hls::stream<ap_axiu<32,1,1,1> >               AXI_STREAM;
     typedef hls::Mat<MAX_HEIGHT,   MAX_WIDTH,   HLS_8UC3> RGB_IMAGE;
