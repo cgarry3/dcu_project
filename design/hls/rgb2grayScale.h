e1e9db44a5721e11b2a5f3c28f9a3ffe9db1be0c
cgarry3 <cathal.garry3@mail.dcu.ie>
2017-12-19 16:11:07 +0000

    C++ header file for rgb2grayScale function

---
+++ design/hls/rgb2grayScale.h
@@ -0,0 +1,18 @@
+#ifndef RGB2GRAYSCALE_H
+#define RGB2GRAYSCALE_H
+
+	#include  "hls_video.h"
+	#include <ap_fixed.h>
+
+    #define MAX_WIDTH  1920
+    #define MAX_HEIGHT 1080
+
+    typedef hls::stream<ap_axiu<32,1,1,1> >               AXI_STREAM;
+    typedef hls::Mat<MAX_HEIGHT,   MAX_WIDTH,   HLS_8UC3> RGB_IMAGE;
+    typedef hls::Mat<MAX_HEIGHT,   MAX_WIDTH,   HLS_8UC1> GRAY_IMAGE;
+
+	void rgb2grayScale_filter(
+		AXI_STREAM& INPUT_STREAM,
+		AXI_STREAM& OUTPUT_STREAM );
+
+#endif
