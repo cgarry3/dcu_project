c8b56b81b6a2b48412bbaf79457cd5b59bf25fe4
cgarry3 <cathal.garry3@mail.dcu.ie>
2018-03-21 13:51:45 +0000

    copying to input image to 0x0f000000 then copies to output image

--- design/eclipse/copy_image_memcpy.cpp
+++ design/eclipse/copy_image_memcpy.cpp
@@ -1,30 +1,49 @@
-#include <opencv2\opencv.hpp>
-
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
-#include <cmath>
+#include <opencv2/opencv.hpp>
+#include <stdio.h>
 #include <iostream>
-#include <opencv2\opencv.hpp>
+#include <stdlib.h>
+
+using namespace cv;
+using namespace std;
+
+typedef unsigned int uint32;
 
 int main()
 {
+
     cv::Mat inImg =    cv::imread("C:\\Users\\cgarry\\Google Drive\\dcu_masters\\project\\design\\image_algorthim\\data\\M6_traffic.jpg");
-    //Data point copy
-    unsigned char * pData = inImg.data;
 
     int width = inImg.rows;
     int height = inImg.cols;
 
+    //Data point copy
+    unsigned char * pData = (unsigned char *)inImg.data;
+    unsigned char * Dest   = (unsigned char *)0x0f000000;;
+    Dest = (unsigned char *)malloc(sizeof(unsigned char)*width*height*3);
+
+
+    //data copy using memcpy function
+    memcpy(Dest, pData, sizeof(unsigned char)*width*height*3);
+
+
+    std::cout << "copied to 0x0f000000 from input image\n";
+
+
     cv::Mat outImg(width, height, CV_8UC3);
     //data copy using memcpy function
-    memcpy(outImg.data, pData, sizeof(unsigned char)*width*height*3);
+    memcpy(outImg.data, Dest, sizeof(unsigned char)*width*height*3);
 
-   //processing and copy check
-   cv::namedWindow("Test");
-   imshow("Test", inImg);
+    std::cout << "copied from 0x0f000000 to output image\n";
 
-   cv::namedWindow("Test2");
-   imshow("Test2", outImg);
+    //processing and copy check
+    cv::namedWindow("Test");
+    imshow("Test", inImg);
 
-   cvWaitKey(0);
+    cv::namedWindow("Test2");
+    imshow("Test2", outImg);
+
+    cvWaitKey(0);
+
+    std::cout << "finished\n";
+
 }
