321c0814ba0de6be8d91b4f4e9ee1507a96274b4
cgarry3 <cathal.garry3@mail.dcu.ie>
2018-04-17 21:07:01 +0100

    Code for testing cma in xilinx linux kernel

---
+++ design/cma/cma_basicTest.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+	int main()
+	{
+		int *ptr_one;
+
+		ptr_one = (int *)cma_alloc(sizeof(int));
+
+		if (ptr_one == 0)
+		{
+			printf("ERROR: Out of memory\n");
+			return 1;
+		}
+
+		*ptr_one = 25;
+		printf("%d\n", *ptr_one);
+
+		cma_free(ptr_one);
+    
+    //unsigned long phy_address = cma_get_phy_addr(ptr_one)
+    //printf("phy address %lu\n", *ptr_one);
+
+		return 0;
+	}
