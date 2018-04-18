6d9a338a7d9b402bc03fb25e406e43904dfb019c
cgarry3 <cathal.garry3@mail.dcu.ie>
2018-04-18 20:11:14 +0100

    Tidying up file

--- design/cma/cma_basicTest.cpp
+++ design/cma/cma_basicTest.cpp
@@ -1,25 +1,25 @@
 #include<stdio.h>
-#include<lib_xlnkcma.h>
+#include "/home/linaro/work/pynq_libs/PYNQ/sdbuild/packages/libsds/xlnkutils/lib_xlnkcma.h"
 
-	int main()
+int main()
+{
+	int *ptr_one;
+
+	ptr_one = (int *)cma_alloc(sizeof(int));
+
+	if (ptr_one == 0)
 	{
-		int *ptr_one;
-
-		ptr_one = (int *)cma_alloc(sizeof(int));
-
-		if (ptr_one == 0)
-		{
-			printf("ERROR: Out of memory\n");
-			return 1;
-		}
-
-		*ptr_one = 25;
-		printf("%d\n", *ptr_one);
-
-		cma_free(ptr_one);
-    
-    //unsigned long phy_address = cma_get_phy_addr(ptr_one)
-    //printf("phy address %lu\n", *ptr_one);
-
-		return 0;
+		printf("ERROR: Out of memory\n");
+		return 1;
 	}
+
+	*ptr_one = 25;
+	printf("%d\n", *ptr_one);
+
+	cma_free(ptr_one);
+
+	//unsigned long phy_address = cma_get_phy_addr(ptr_one)
+	//printf("phy address %lu\n", *ptr_one);
+
+	return 0;
+}
