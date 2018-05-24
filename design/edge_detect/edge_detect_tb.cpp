ed84ebd08012036eaa6099cdc4a99f1b4fe31ac7
cgarry3 <cathal.garry3@mail.dcu.ie>
2018-05-24 19:28:22 +0100

    updates to testbench

--- design/edge_detect/edge_detect_tb.cpp
+++ design/edge_detect/edge_detect_tb.cpp
@@ -1,16 +1,26 @@
-#include "edge_detect.h"
+#include "edge_dect.h"
 #include "hls_opencv.h"
+#include "iostream"
+
+using namespace std;
 
 int main()
 {
+    // Width and height of image
     int const rows = MAX_HEIGHT;
     int const cols = MAX_WIDTH;
+
+    // load image
     cv::Mat src = cv::imread(INPUT_IMAGE);
     cv::Mat dst = src;
+
+    // DUT
     stream_t stream_in, stream_out;
     cvMat2AXIvideo(src, stream_in);
     edge_detect(stream_in, stream_out);
     AXIvideo2cvMat(stream_out, dst);
+
+    // write out image
     cv::imwrite(OUTPUT_IMAGE, dst);
     return 0;
 }
