f56c8adb3622f495073af1392db667a3e12c19f2
cgarry3 <cathal.garry3@mail.dcu.ie>
2018-05-24 19:27:04 +0100

    updating edge detect to include dilucation
    gaussian blur can be included with 
    
        hls::GaussianBlur<5,5>(img1, img2);

--- design/edge_detect/edge_detect.cpp
+++ design/edge_detect/edge_detect.cpp
@@ -1,16 +1,36 @@
-#include "edge_detect.h"
+#include "edge_dect.h"
 
 void edge_detect(stream_t& stream_in, stream_t& stream_out)
 {
+	// directives
+	#pragma HLS INTERFACE axis register both port=stream_out
+	#pragma HLS INTERFACE axis register both port=stream_in
+    #pragma HLS dataflow
+
+	// Image width and height
     int const rows = MAX_HEIGHT;
     int const cols = MAX_WIDTH;
+
+    // local storage
     rgb_img_t img0(rows, cols);
     rgb_img_t img1(rows, cols);
     rgb_img_t img2(rows, cols);
     rgb_img_t img3(rows, cols);
+    rgb_img_t img4(rows, cols);
+    rgb_img_t img5(rows, cols);
+
+    // turn gray
     hls::AXIvideo2Mat(stream_in, img0);
     hls::CvtColor<HLS_RGB2GRAY>(img0, img1);
-    hls::Sobel<1,0,3>(img1, img2);
-    hls::CvtColor<HLS_GRAY2RGB>(img2, img3);
-    hls::Mat2AXIvideo(img3, stream_out);
+
+    // dilate blur
+    hls::Dilate(img1, img2);
+
+    // detect edge
+    hls::Sobel<1,0,3>(img2, img3);
+    hls::CvtColor<HLS_GRAY2RGB>(img3, img4);
+
+    // output image
+    hls::Mat2AXIvideo(img4, stream_out);
 }
+
