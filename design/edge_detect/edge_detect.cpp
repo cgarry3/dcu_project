3bf405db195aeab8151623df9c70a7595d1854ff
cgarry3 <cathal.garry3@mail.dcu.ie>
2018-06-14 11:32:34 +0100

    Updating edge detector IP

--- design/edge_detect/edge_detect.cpp
+++ design/edge_detect/edge_detect.cpp
@@ -1,40 +1,50 @@
+// -------------------------------------------------------------------------
+//       DCU Custom Edge Detector Filter
+//
+//  Author:       Cathal Garry
+//  Description:  This is a custom edge detector filter for
+//                720P images. It contains an input and output AXI stream
+//                ports.
+// ------------------------------------------------------------------------
+
 #include "edge_dect.h"
 
- 
-void edge_detect(stream_t& stream_in, stream_t& stream_out)
+void edge_detect(AXI_STREAM& stream_in, AXI_STREAM& stream_out)
 {
 	// directives
 	#pragma HLS INTERFACE axis register both port=stream_out
 	#pragma HLS INTERFACE axis register both port=stream_in
+
+	// Removes ap_ctrl interface
+	#pragma HLS INTERFACE ap_ctrl_none port=return
+
+	// Synthesis data flow improvement
     #pragma HLS dataflow
 
- 
 	// Image width and height
     int const rows = MAX_HEIGHT;
     int const cols = MAX_WIDTH;
 
- 
     // local storage
-    rgb_img_t img0(rows, cols);
-    rgb_img_t img1(rows, cols);
-    rgb_img_t img2(rows, cols);
-    rgb_img_t img3(rows, cols);
-    rgb_img_t img4(rows, cols);
-    rgb_img_t img5(rows, cols);
+    RGB_IMAGE  imgInput(rows, cols);
+    GRAY_IMAGE imgGray(rows, cols);
+    GRAY_IMAGE imgDilate(rows, cols);
+    GRAY_IMAGE imgEdges(rows, cols);
+    RGB_IMAGE  imgOutput(rows, cols);
 
- 
+    // convert input stream
+    hls::AXIvideo2Mat(stream_in, imgInput);
+
     // turn gray
-    hls::AXIvideo2Mat(stream_in, img0);
-    hls::CvtColor<HLS_RGB2GRAY>(img0, img1);
+    hls::CvtColor<HLS_RGB2GRAY>(imgInput, imgGray);
 
- 
     // dilate blur
-    hls::Dilate(img1, img2);
+    hls::Dilate(imgGray, imgDilate);
 
     // detect edge
-    hls::Sobel<1,0,3>(img2, img3);
-    hls::CvtColor<HLS_GRAY2RGB>(img3, img4);
+    hls::Sobel<1,0,3>(imgDilate, imgEdges);
+    hls::CvtColor<HLS_GRAY2RGB>(imgEdges, imgOutput);;
 
     // output image
-    hls::Mat2AXIvideo(img4, stream_out);
+    hls::Mat2AXIvideo(imgOutput, stream_out);
 }
