6c482d90837e9583a75fe1723fbdf6058984d74f
cgarry3 <cathal.garry3@mail.dcu.ie>
2018-06-27 09:32:31 +0100

    adding loop to allow for continuous publishing

--- design/mqtt/publish.py
+++ design/mqtt/publish.py
@@ -1,11 +1,30 @@
+#############################################################
+#  Name:          MQTT Publisher
+#  Aurthor:       Cathal Garry
+#  Description:   The MQTT publisher reads the number
+#                 cars that have passed since the last
+#                 message and publishes it to the broker
+#############################################################
+
 import paho.mqtt.client as mqtt
-message = 'ON'
+import time
+
+## rate of messages published per second
+publishRate = 1
+
+def setupHDMIPipe():
+    ## add code here for setting up HDMI pipe
+    
+def readResultReg():
+    ## add code here to read result from 
+    ## custom IP in HDMI pipe
+
 def on_connect(mosq, obj, rc):
     mqttc.subscribe("f", 0)
     print("rc: " + str(rc))
 
 def on_message(mosq, obj, msg):
-    global message
+    message = '{d:{Number of Vehicles:' + randint(0, 5) + '}}'
     print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
     message = msg.payload
     mqttc.publish("f2",msg.payload);
@@ -20,14 +39,20 @@ def on_log(mosq, obj, level, string):
     print(string)
 
 mqttc = mqtt.Client()
-# Assign event callbacks
-mqttc.on_message = on_message
-mqttc.on_connect = on_connect
-mqttc.on_publish = on_publish
-mqttc.on_subscribe = on_subscribe
-# Connect
-mqttc.connect("localhost", 1883,60)
+
+while True:
+    # Assign event callbacks
+    mqttc.on_message = on_message
+    mqttc.on_connect = on_connect
+    mqttc.on_publish = on_publish
+    mqttc.on_subscribe = on_subscribe
+    
+    # Connect
+    mqttc.connect("localhost", 1883,60)
+    
+    # sleep
+    time.sleep(publishRate)
 
 
 # Continue the network loop
-mqttc.loop_forever()
+#mqttc.loop_forever()
