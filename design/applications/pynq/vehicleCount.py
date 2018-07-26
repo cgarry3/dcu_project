673e49bdc96b94975d05d2bb3a10c12097dedbd1
cgarry3 <cathal.garry3@mail.dcu.ie>
2018-07-26 11:56:09 +0100

    adding congestion information to pubisher

--- design/applications/pynq/vehicleCount.py
+++ design/applications/pynq/vehicleCount.py
@@ -17,6 +17,13 @@ from pynq.lib.video import *
 from collections import deque
 
 ######################################
+## Debug Mode
+##  - Set to one to print debug msg
+######################################
+
+DEBUG = 0
+
+######################################
 ## Video Processing Variables
 ######################################
 
@@ -28,7 +35,7 @@ frameRatePerSecond = 25
 timePerFrame = 1/frameRatePerSecond
 
 ## Path to Overlay
-overlayPath = "/home/xilinx/jupyter_notebooks/overlays/debug/debug8/debug8.bit"
+overlayPath = "/home/xilinx/jupyter_notebooks/overlays/1080p/hdmi_only/motorway/motorway.bit"
 
 ######################################
 ## MQTT Variables
@@ -55,8 +62,13 @@ timeBetweenPublishs = publishRate/timePerFrame
 
 IP_BASE_ADDRESS = 0x43C70000
 ADDRESS_RANGE = 0x10000
-RESULT_REG_OFFSET = 0x10
+RESULT_REG_OFFSET = 0x30
+LEFT_COUNT_REG_OFFSET = 0x18
+RIGHT_COUNT_REG_OFFSET = 0x28
 
+######################################
+## Congestion Variables
+######################################
 
 ## Lane selection initialization
 laneSelect = []
@@ -64,12 +76,15 @@ for i in range(numOfLanes):
     singleLaneSelect = 1 << i;
     laneSelect.append(singleLaneSelect)
     
-## Vehicle counts
-presentVehicleCount = []
-previousVehicleCount = []
+## Congestion Delays
+congestionDelays = []
 for i in range(numOfLanes):
-    presentVehicleCount.append(0)
-    previousVehicleCount.append(0)
+    congestionDelays.append(0)
+    
+## Vehicle counts
+VehicleCount = []
+for i in range(numOfLanes):
+    VehicleCount.append(0)
     
 ## Used to determine if a new vehicle has passed
 numOfFramesChecked = 8
@@ -97,13 +112,21 @@ def setupHDMIPipe():
 
     ## Connecting HDMI directly to HDMI out
     ol.video.hdmi_in.tie(ol.video.hdmi_out)
-    time.sleep(120)
+    time.sleep(30)
     
 def closeHDMIPipe():
     ## Closing HDMI stream In&Out
     ol.video.hdmi_out.close()
     ol.video.hdmi_in.close()
 
+def readLeftCountReg():
+    result = mmio.read(LEFT_COUNT_REG_OFFSET)
+    return result;
+    
+def readRightCountReg():
+    result = mmio.read(RIGHT_COUNT_REG_OFFSET)
+    return result;
+ 
 def readResultReg():
     result = mmio.read(RESULT_REG_OFFSET)
     return result;
@@ -156,10 +179,12 @@ def mqttSetup(ipAddress, portNum, username, pwd):
 #####################################
 
 ## create client objec
+print("############### Setting up MQTTT Publisher ###############")
 mqttc = mqtt.Client()
 
 ## setup MQTT publisher
 mqttSetup(brokerIP, port, user, password)
+
 
 #####################################
 ## Setup Video Pipeline
@@ -169,19 +194,27 @@ mqttSetup(brokerIP, port, user, password)
 ol = pynq.Overlay(overlayPath)
 
 ## Setup HDMI Pipeline
+print("############### Setting up HDMI Pipeline ###############")
 setupHDMIPipe()
+
 
 #####################################
 ## Register Read Setup
 #####################################
 
+print("############### Setting up Register Access ###############")
 mmio       = MMIO(IP_BASE_ADDRESS, ADDRESS_RANGE)
 frameCount = 0
 
-## processing result from Custom IP
-while frameCount<200:
+####################################################
+## Determining congestion based off result value
+####################################################
+
+while True:
+    ## Result readback value
     result = readResultReg()
     
+
     ###########################
     ## Count Vehicles
     ###########################
@@ -192,6 +225,10 @@ while frameCount<200:
         allLanesResults[x].pop(0)
         ## add new result
         allLanesResults[x].append(resultTemp)
+
+        ##################################
+        # Count
+        ##################################
         
         ## check if a new vehicle has crossed the count line
         newVehicleCrossLine = False
@@ -203,24 +240,61 @@ while frameCount<200:
         
         ## Increase count
         if(newVehicleCrossLine==True):
-            presentVehicleCount[x]=presentVehicleCount[x]+1
-            print("Lane" + str(x) + " count: " + str(presentVehicleCount[x]))
-            
-        print("all results" + str(x) + ":")
-        print(allLanesResults[x])
-    print("all vechicle counts:")
-    print(presentVehicleCount)
+            VehicleCount[x]=VehicleCount[x]+1
+            if(DEBUG==1):
+                print("Lane" + str(x) + " count: " + str(VehicleCount[x]))
+       
+       
+        ##################################
+        ## Increase congestion count
+        ##  - congestion count increase will there is a car being tracked
+        ##  - when all the list==0 the car has passed
+        ##################################
         
+        if(sum(allLanesResults[x])==0 and congestionDelays[x]!=0):
+            if(DEBUG==1):
+                print("Lane" + str(x) + " Max Delay: " + str(congestionDelays[x]))
+            congestionDelays[x]=0
+        else:
+            congestionDelays[x]=congestionDelays[x]+1
+        
+        if(DEBUG==1):    
+            print("all results" + str(x) + ":")
+            print(allLanesResults[x])
+            print(congestionDelays[x])
+    if(DEBUG==1):
+        print("Current count")
+        print(VehicleCount)
     
     ###########################
     ## Publish Results
     ###########################
     
     if(frameCount==timeBetweenPublishs):
-        vehCount=  sum(presentVehicleCount[1:3])
-        topic   =  'ee580/m3P0'
-        message = '{d:{Number of Vehicles:' + str(random.randint(0, 5)) + '}}'
+        ## print out 
+        print("############### Publishing Data ###############")
+
+        ## Read Count values
+        leftLaneCount =sum(VehicleCount[0:2])
+        rightLaneCount =sum(VehicleCount[3:5])
+
+        ## Publish left lane count
+        topic   =  'ee580/m3p0_left_count'
+        message = '{d:{Left Lane Count:' + str(leftLaneCount) + '}}'
         mqttc.publish(topic,message)
+        
+        ## Publish right lane count
+        topic   =  'ee580/m3p0_right_count'
+        message = '{d:{Right Lane Count:' + str(rightLaneCount) + '}}'
+        mqttc.publish(topic,message)
+        
+        ## Publish max congestion(mseconds)
+        maxCongestion = max(congestionDelays)
+        topic   =  'ee580/m3p0_congestion'
+        message = '{d:{Max Congestion:' + str(int(maxCongestion*timePerFrame)) + '}}'
+        mqttc.publish(topic,message)
+        
+        ## Reset frame count
         frameCount = 0
 
     ###########################
